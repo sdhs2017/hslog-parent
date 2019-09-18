c8458908a72b957a4366579cad40bd7a59d62658
sdhs2017 <sdhslog2017@163.com>
2019-09-18 15:45:02 +0800

    hslog es5 searchTemplate edit

--- elasticsearch5/src/main/java/com/hs/elsearch/template/ESTransportSearchTemplate.java
+++ elasticsearch5/src/main/java/com/hs/elsearch/template/ESTransportSearchTemplate.java
@@ -6,10 +6,15 @@ import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.aggregations.AggregationBuilder;
+import org.elasticsearch.search.aggregations.Aggregations;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
+import org.elasticsearch.search.sort.SortBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -27,7 +32,181 @@ public class ESTransportSearchTemplate {
     TransportClient transportClient;
 
     /**
-     *
+     * 通过查询条件获取count
+     * @param types
+     * @param queryBuilder
+     * @param indices
+     * @return
+     * 获取index+type下的索引数据条数
+     */
+    public long getCountByQuery(QueryBuilder queryBuilder,String [] types,String... indices) {
+        SearchRequestBuilder searchRequestBuilder = transportClient.prepareSearch(indices);
+        if (types!=null&&types.length>0) {
+            searchRequestBuilder.setTypes(types);
+        }
+        if (queryBuilder!=null) {
+            searchRequestBuilder.setQuery(queryBuilder);
+        }
+        SearchResponse response = searchRequestBuilder.get();
+        long length = response.getHits().getTotalHits();
+        return length;
+    }
+
+    /**
+     * 带有条件的聚合查询
+     * @param queryBuilder
+     * @param aggregationBuilder
+     * @param types
+     * @param indices
+     * @return
+     */
+    public Aggregations getAggregationsByBuilder(QueryBuilder queryBuilder,AggregationBuilder aggregationBuilder,String [] types,String... indices){
+
+        SearchRequestBuilder requestBuilder = transportClient.prepareSearch(indices);
+        if (types!=null&&types.length>0){
+            requestBuilder.setTypes(types);
+        }
+        if (queryBuilder!=null){
+            requestBuilder.setQuery(queryBuilder);
+        }
+        if (aggregationBuilder!=null){
+            requestBuilder.addAggregation(aggregationBuilder);
+        }
+
+        SearchResponse response = requestBuilder.get();
+
+        Aggregations aggregations= response.getAggregations();
+
+        return aggregations;
+    }
+
+    /**
+     * 聚合查询
+     * @param aggregationBuilder
+     * @param types
+     * @param indices
+     * @return
+     */
+    public Aggregations getAggregationsByBuilder(AggregationBuilder aggregationBuilder, String [] types, String... indices){
+
+        SearchRequestBuilder requestBuilder = transportClient.prepareSearch(indices);
+        if (types!=null&&types.length>0){
+            requestBuilder.setTypes(types);
+        }
+        if (aggregationBuilder!=null){
+            requestBuilder.addAggregation(aggregationBuilder);
+        }
+
+        SearchResponse response = requestBuilder.get();
+
+        Aggregations aggregations = response.getAggregations();
+
+        return aggregations;
+    }
+
+    /**
+     * 通过查询体（QueryBuilder）获取数据（带排序功能）
+     * 带有排序的分页查询，带有高亮
+     * @param queryBuilder 查询体
+     * @param sortBuilder 排序设置
+     * @param highlightBuilder 高亮内容设置
+     * @param from 翻页
+     * @param size 每页数据条数
+     * @param types
+     * @param indices
+     * @return
+     */
+    public List<Map<String, Object>> getListByBuilder(QueryBuilder queryBuilder, SortBuilder sortBuilder, HighlightBuilder highlightBuilder, int from, int size, String [] types, String... indices){
+
+        SearchRequestBuilder requestBuilder = transportClient.prepareSearch(indices);
+        if (types!=null&&types.length>0){
+            requestBuilder.setTypes(types);
+        }
+        if (queryBuilder!=null){
+            requestBuilder.setQuery(queryBuilder);
+        }
+        if (sortBuilder!=null){
+            requestBuilder.addSort(sortBuilder);
+        }
+        if (highlightBuilder!=null){
+            requestBuilder.highlighter(highlightBuilder);
+        }
+        if (from!=-1&&size!=-1){
+            requestBuilder.setFrom(from).setSize(size);
+        }
+
+        SearchResponse response = requestBuilder.get();
+
+        SearchHits searchHits = response.getHits();
+
+        return getListBySearchHit(searchHits);
+    }
+
+    /**
+     * 通过查询体（QueryBuilder）获取数据（带排序功能）
+     * 带有排序的分页查询
+     * @param queryBuilder 查询体
+     * @param sortBuilder 排序体
+     * @param from 翻页
+     * @param size 每页数据条数
+     * @param types
+     * @param indices
+     * @return
+     */
+    public List<Map<String, Object>> getListByBuilder(QueryBuilder queryBuilder, SortBuilder sortBuilder,int from,int size, String [] types, String... indices){
+
+        SearchRequestBuilder requestBuilder = transportClient.prepareSearch(indices);
+        if (types!=null&&types.length>0){
+            requestBuilder.setTypes(types);
+        }
+        if (queryBuilder!=null){
+            requestBuilder.setQuery(queryBuilder);
+        }
+        if (sortBuilder!=null){
+            requestBuilder.addSort(sortBuilder);
+        }
+//        if (from!=-1&&size!=-1){
+        if (from>=0&&size>=0){
+            requestBuilder.setFrom(from).setSize(size);
+        }
+
+        SearchResponse response = requestBuilder.get();
+
+        SearchHits searchHits = response.getHits();
+
+        return getListBySearchHit(searchHits);
+    }
+
+    /**
+     * 通过查询体（QueryBuilder）获取数据（带排序功能）
+     * @param queryBuilder
+     * @param sortBuilder
+     * @param types
+     * @param indices
+     * @return
+     */
+    public List<Map<String, Object>> getListByBuilder(QueryBuilder queryBuilder, SortBuilder sortBuilder, String [] types, String... indices){
+
+        SearchRequestBuilder requestBuilder = transportClient.prepareSearch(indices);
+        if (types!=null&&types.length>0){
+            requestBuilder.setTypes(types);
+        }
+        if (queryBuilder!=null){
+            requestBuilder.setQuery(queryBuilder);
+        }
+        if (sortBuilder!=null){
+            requestBuilder.addSort(sortBuilder);
+        }
+
+        SearchResponse response = requestBuilder.get();
+
+        SearchHits searchHits = response.getHits();
+
+        return getListBySearchHit(searchHits);
+    }
+
+    /**
+     * 通过查询体（QueryBuilder）获取数据
      * @param queryBuilder
      * @param types
      * @param indices
@@ -47,7 +226,8 @@ public class ESTransportSearchTemplate {
     }
 
     /**
-     *
+     * 通过SearchHit数组获取数据，补全index、type、id，组成list，返回
+     * TODO 确认api是否有直接返回集合的方法
      * @param searchHits
      * @return
      */
@@ -59,6 +239,18 @@ public class ESTransportSearchTemplate {
             map.put("index", hit.getIndex());
             map.put("type", hit.getType());
             map.put("id", hit.getId());
+            list.add(map);
+        }
+        return list;
+    }
+    public List<Map<String, Object>> getListBySearchHit2(SearchHits searchHits){
+        SearchHit[] searchHit = searchHits.getHits();
+        List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+        for(SearchHit hit : searchHit) {
+            Map<String, Object> map = hit.getSource();
+//            map.put("index", hit.getIndex());
+//            map.put("type", hit.getType());
+//            map.put("id", hit.getId());
             list.add(map);
         }
         return list;

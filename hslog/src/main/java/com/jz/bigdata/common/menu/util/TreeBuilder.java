e53a145c0a74d5c99bf6b6cd7f1e7876229cd47d
sdhs2017 <sdhslog2017@163.com>
2019-09-30 15:16:55 +0800

    hslog menu,auth update by savilio

--- hslog/src/main/java/com/jz/bigdata/common/menu/util/TreeBuilder.java
+++ hslog/src/main/java/com/jz/bigdata/common/menu/util/TreeBuilder.java
@@ -13,6 +13,11 @@ import com.jz.bigdata.common.menu.entity.Menu;
  * @date 2017年8月9日 上午9:39:45
  * @description 拼接json樹
  */
+
+/**
+ * TODO
+ * JDK8新特性：构建tree
+ */
 public class TreeBuilder {
 	// 获取list集合信息
 	List<Menu> menuList = new ArrayList<Menu>();
@@ -25,7 +30,7 @@ public class TreeBuilder {
 
 	/**
 	 * @return
-	 * 
+	 *
 	 * 		list转json 构建树形结构
 	 */
 	public String buildJSONTree() {
@@ -105,7 +110,7 @@ public class TreeBuilder {
 			}
 
 		}
-		
+
 		return isRootNode;
 	}
 
@@ -157,7 +162,7 @@ public class TreeBuilder {
 				return 0; // 相等为0
 			}
 		});
-		
+
 		return childNodes;
 	}
 
--- hslog/src/main/java/com/jz/bigdata/util/AuthInterceptor.java
+++ hslog/src/main/java/com/jz/bigdata/util/AuthInterceptor.java
@@ -26,119 +26,157 @@ import com.jz.bigdata.common.users.service.IUsersService;
  * @description controller请求拦截器，请求到达controller前
  */
 public class AuthInterceptor extends HandlerInterceptorAdapter {
-	
+
 	@Resource(name = "UsersService")
 	private IUsersService userService;
-	
+
 	@Resource(name = "FunctionService")
 	private IFunctionService functionService;
-	
+
 	@Resource
 	private IUsersDao userDao;
-	
+
 	@Resource
 	private IFunctionDao functionDao;
-    
-    /**
-     * @param request
-     * @param response
-     * @param handler
-     * @return
-     * @throws Exception
-     * @description
-     * 拦截器
-     */
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
-    	//如果是登陆请求，放行
-    	if(handler.toString().indexOf(Constant.LOGINPATH)>=0||handler.toString().indexOf(Constant.REGISTERPATH)>=0||handler.toString().indexOf(Constant.uploadPATH)>=0){
-    		//退出登录，注册，上传不拦截
-    		if(request.getRequestURI().equals("/hslog/users/logout.do")||request.getRequestURI().equals("/hslog/users/registerUser.do")||request.getRequestURI().equals("/hslog/upload/licenseUpload.do")){
-    			return true;
-    		}else{
-    		String phone=request.getParameter("phone");
-    		String password=request.getParameter("password");
-    		User user =new User();
-    		user.setPhone(phone);
-    		user.setPassword(MD5.EncoderByMd5(password));
-    		//查询用户
-    		List<User> _userList = userDao.selectByPhonePwd(user);
-    		
+	/**
+	 * @param request
+	 * @param response
+	 * @param handler
+	 * @return
+	 * @throws Exception
+	 * @description
+	 * 拦截器
+	 */
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+		//如果是登陆请求，放行
+		if(handler.toString().indexOf(Constant.LOGINPATH)>=0||handler.toString().indexOf(Constant.REGISTERPATH)>=0||handler.toString().indexOf(Constant.uploadPATH)>=0){
+			//退出登录，注册，上传不拦截
+			/**
+			 * TODO
+			 * equals改为indexof(*)>0
+			 * 或者 只判断项目路径后面的相对路径
+			 */
+			if(request.getRequestURI().equals("/jzLog/users/logout.do")||request.getRequestURI().equals("/jzLog/users/registerUser.do")||request.getRequestURI().equals("/jzLog/upload/licenseUpload.do")){
+				return true;
+			}else{
+				String phone=request.getParameter("phone");
+				String password=request.getParameter("password");
+				User user =new User();
+				user.setPhone(phone);
+				user.setPassword(MD5.EncoderByMd5(password));
+				//查询用户
+				List<User> _userList = userDao.selectByPhonePwd(user);
+
 //    		System.out.println(_userList.get(0).getName());
-    		GetfunctionMap getfunctionMap= new GetfunctionMap();
+				GetfunctionMap getfunctionMap= new GetfunctionMap();
 //    		getfunctionMap.map;
-    		//获取权限列表
-    		Map<String,Map<String,String>> map=GetfunctionMap.map;
+				//获取权限列表
+				/**
+				 * TODO
+				 * 后期修改
+				 * 1.多线程隐患：GetfunctionMap.map应声明为线程安全的Map
+				 * 2.不使用静态方式声明，不使用对象.成员变量形式引用
+				 */
+				Map<String,Map<String,String>> map=GetfunctionMap.map;
 //    		System.out.println(map.get(_userList.get(0).getRole()));
-    		
-    		if(_userList.size()<1){
-    			return true;
-    		}else{
-    			//判断缓存中是否有数据
-    			if(map.get(String.valueOf(_userList.get(0).getRole()) )==null||map.get(String.valueOf(_userList.get(0).getRole())).equals("")){
-    				getfunctionMap.getfunctionMap(_userList.get(0).getRole(),functionService);
-    			}
-    		}
+
+				if(_userList.size()<1){
+					/**
+					 * TODO
+					 * true为放行
+					 * 接口为boolean，无法设置返回内容，所以放行，在UserServiceImpl的login里处理返回
+					 * 也可以用以下方式返回
+					 * out.print("{\"success\":\"false\",\"message\":\"登录超时，请刷新页面重新登录\"}");
+					 */
+					return true;
+				}else{
+					//判断缓存中是否有数据
+					/**
+					 * TODO
+					 * 后期修改：
+					 * function的map加载改为初始全部加载
+					 */
+					if(map.get(String.valueOf(_userList.get(0).getRole()) )==null||map.get(String.valueOf(_userList.get(0).getRole())).equals("")){
+						getfunctionMap.getfunctionMap(_userList.get(0).getRole(),functionService);
+					}
+				}
 //    		System.out.println(GetfunctionMap.map.size());
 //    		funService.map;
 //    		functionService.getClass()
 //    		functionService
 //    	    functionService.selectAll(_userList.get(0).getRole());
-    	    
+
 //    		System.err.println(request.getRequestURI());
 //    		PrintWriter out = response.getWriter();
 //    		response.setCharacterEncoding("utf-8");
-//            out.print("您没有权限");  
-             
-    		return true;
-    		}
-    	}else{
-    		//session超时检测
-        	HttpSession session = request.getSession();
-        	//session userid不为空且sessionid一致
+//            out.print("您没有权限");
+
+				return true;
+			}
+		}else{
+			//session超时检测
+			HttpSession session = request.getSession();
+			//session userid不为空且sessionid一致
 //        	System.err.println(session.getAttribute(Constant.SESSION_USERID)!=null);
 //        	System.err.println(session.getId().equals(session.getAttribute(Constant.SESSION_ID)));
 //        	System.err.println(session.getAttribute(Constant.SESSION_USERID));
 //        	System.err.println(session.getAttribute(Constant.SESSION_ID));
-        	//判断session是否超时
-        	if(session.getAttribute(Constant.SESSION_USERID)!=null&&session.getId().equals(session.getAttribute(Constant.SESSION_ID))){
+			//判断session是否超时
+			if(session.getAttribute(Constant.SESSION_USERID)!=null&&session.getId().equals(session.getAttribute(Constant.SESSION_ID))){
 //        		System.err.println(session.getAttribute(Constant.SESSION_USERID));
-        		Map<String,Map<String,String>> map=GetfunctionMap.map;
+				Map<String,Map<String,String>> map=GetfunctionMap.map;
 //        		System.out.println(Constant.SESSION_USERROLE);
 //        		System.out.println(session.getAttribute(Constant.SESSION_USERROLE));
 //        		System.out.println(map.get(session.getAttribute(Constant.SESSION_USERROLE).toString()));
 //        		System.out.println(request.getRequestURI().toString());
-        		//是否有权限
-        		if(map!=null&&map.get(session.getAttribute(Constant.SESSION_USERROLE).toString()).get(request.getRequestURI().toString())!=null){
-        			return true;
-        		//无权限	
-        		}else{
-        			response.setContentType("text/html;charset=utf-8");
-        			PrintWriter out = response.getWriter();
+				//是否有权限
+				/**
+				 * TODO
+				 * request.getRequestURI().toString()为带项目名的路径，
+				 * 如果数据库function表中的resource字段后期删除项目名，则此处要截取掉项目名
+				 */
+				if(map!=null&&map.get(session.getAttribute(Constant.SESSION_USERROLE).toString()).get(request.getRequestURI().toString())!=null){
+					return true;
+					//无权限
+				}else{
+					response.setContentType("text/html;charset=utf-8");
+					PrintWriter out = response.getWriter();
 //        			JSONObject jsStr = JSONObject.parseObject("{msg:您没有权限}");
 //        			JSONArray array=JSONArray.fromObject("{msg:您没有权限}");
 //            		response.setCharacterEncoding("utf-8");
-                    out.print("您没有权限");  
-                    
-        			return false;
-        		}
+
+					/**
+					 * TODO
+					 * 规范返回信息，与前端回调匹配
+					 */
+					out.print("您没有权限");
+
+					return false;
+				}
 //        		System.out.println(map.size());
 //        		System.out.println(map.get(session.getAttribute(Constant.SESSION_USERROLE)).get("/jzLog/users/selectUser.do"));
 //        		return true;
-        	//无权限
-        	}else{
-        		response.setContentType("text/html;charset=utf-8");
-    			PrintWriter out = response.getWriter();
+				//无权限
+			}else{
+				response.setContentType("text/html;charset=utf-8");
+				PrintWriter out = response.getWriter();
 //    			JSONObject jsStr = JSONObject.parseObject("{msg:您没有权限}");
 //    			JSONArray array=JSONArray.fromObject("{msg:您没有权限}");
 //        		response.setCharacterEncoding("utf-8");
-                out.print("{\"success\":\"false\",\"message\":\"登录超时，请刷新页面重新登录\"}");  
-                return false;
-            }     
-    	}
-          
-     }
-    
+
+				/**
+				 * TODO
+				 * 验证返回信息是否规范
+				 */
+				out.print("{\"success\":\"false\",\"message\":\"登录超时，请刷新页面重新登录\"}");
+				return false;
+			}
+		}
+
+	}
+
 
 }

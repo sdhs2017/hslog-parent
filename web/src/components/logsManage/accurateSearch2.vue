8cec7a3967f348672f72137ef7c3ba555eb196ee
honglian0228 <1012156899@qq.com>
2020-05-16 09:48:33 +0800

    时间参数

--- web/src/components/logsManage/accurateSearch2.vue
+++ web/src/components/logsManage/accurateSearch2.vue
@@ -273,8 +273,21 @@
         watch: {
             '$route' (to, from) { //监听路由是否变化
                 if (this.$route.params.logLevel) {
+                    let st = ''
+                    let et = ''
+                    if(this.$route.params.dateArr[0].length){
+                        st = this.$route.params.dateArr[0]+ ' 00:00:00'
+                        et = this.$route.params.dateArr[1]+ ' 23:59:59'
+                    }else{
+                        st = dateFormat('yyyy-mm-dd',this.$route.params.dateArr[0])+ ' 00:00:00'
+                        et = dateFormat('yyyy-mm-dd',this.$route.params.dateArr[1])+ ' 23:59:59'
+                    }
                     this.formConditionsArr[2].model.model=[this.$route.params.logLevel];
+                    this.formConditionsArr[0].model.model=[st,et]
                     this.searchConditions['log.level']=this.$route.params.logLevel;
+                    this.searchConditions.starttime = st;
+                    this.searchConditions.endtime = et;
+                    //清空日志类型
                     this.formConditionsArr[1].model.model = ''
                     this.searchConditions['agent.type'] = ''
                 }

9d4ecafa37acb6df9637b6bd066d3e86c75c1876
zhangyiyang <zhang123>
2020-05-25 09:28:55 +0800

    search.max_buckets配置

---
+++ elasticsearch7/src/main/java/com/hs/elsearch/dao/globalDao/IGlobalDao.java
@@ -0,0 +1,23 @@
+package com.hs.elsearch.dao.globalDao;
+
+import org.elasticsearch.action.admin.cluster.settings.ClusterGetSettingsResponse;
+
+import java.util.Map;
+
+public interface IGlobalDao {
+    /**
+     * 设置cluster-setting信息
+     * @param persistentMap
+     * @param transientMap
+     * @return
+     * @throws Exception
+     */
+    public boolean updateClusterSetting(Map<String,Object> persistentMap , Map<String,Object> transientMap) throws Exception;
+
+    /**
+     * 获取cluster-setting信息
+     * @return
+     * @throws Exception
+     */
+    public ClusterGetSettingsResponse getClusterSetting() throws Exception;
+}
---
+++ elasticsearch7/src/main/java/com/hs/elsearch/dao/globalDao/impl/GlobalDao.java
@@ -0,0 +1,22 @@
+package com.hs.elsearch.dao.globalDao.impl;
+
+import com.hs.elsearch.dao.globalDao.IGlobalDao;
+import com.hs.elsearch.template.GlobalTemplate;
+import org.elasticsearch.action.admin.cluster.settings.ClusterGetSettingsResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Map;
+
+public class GlobalDao implements IGlobalDao {
+    @Autowired
+    GlobalTemplate globalTemplate;
+    @Override
+    public boolean updateClusterSetting(Map<String, Object> persistentMap, Map<String, Object> transientMap) throws Exception {
+        return globalTemplate.updateClusterSetting(persistentMap,transientMap);
+    }
+
+    @Override
+    public ClusterGetSettingsResponse getClusterSetting() throws Exception {
+        return globalTemplate.getClusterInfo();
+    }
+}

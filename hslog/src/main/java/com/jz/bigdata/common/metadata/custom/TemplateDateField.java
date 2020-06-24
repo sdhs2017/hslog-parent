478e70e4733f8bf6adcd0ce0c335138074cf9513
zhangyiyang <zhang123>
2020-06-24 10:26:28 +0800

    tempalte与日期字段对应

---
+++ hslog/src/main/java/com/jz/bigdata/common/metadata/custom/TemplateDateField.java
@@ -0,0 +1,16 @@
+package com.jz.bigdata.common.metadata.custom;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 定义template与日期字段的对饮关系
+ */
+public enum TemplateDateField {
+    INSTANCE;
+    public final static Map<String, String> TemplateDateField = new HashMap<>();
+    static{
+        //TemplateDateField.put(0, "emergency");
+
+    }
+}

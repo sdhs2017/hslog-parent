5e0ad85bc8732aed43602c90afe7056e5d6394ef
honglian0228 <1012156899@qq.com>
2020-10-23 16:35:21 +0800

    添加告警 修改

--- web/src/components/alarmManage/alarmList.vue
+++ web/src/components/alarmManage/alarmList.vue
@@ -6,16 +6,220 @@
                 <el-button type="success" size="mini" plain  @click="refresh">刷新</el-button>
             </div>
         </div>
+        <div class="search-wapper">
+            <v-search-form :formItem="formConditionsArr" :busName="busName"></v-search-form>
+        </div>
+        <div class="alarm-list-wapper">
+            <vBasetable :tableHead="tableHead" :tableData="tableData"></vBasetable>
+        </div>
+        <div class="alarm-table-page">
+            <span>共检索到告警数量为 <b>{{allCounts}}</b> 个</span>
+            <el-pagination background layout="prev, pager, next" @current-change="handleCurrentChange" :current-page.sync="c_page" :page-size="size" :total="allCounts"></el-pagination>
+        </div>
     </div>
 </template>
 
 <script>
+    import vSearchForm from '../common/BaseSearchForm';
+    import vBasetable from '../common/Basetable2';
+    import {jumpHtml} from "../../../static/js/common";
+    import bus from '../common/bus';
     export default {
         name: "alarmList",
         data(){
             return{
-                loading:false
+                loading:false,
+                page:1,//页码
+                c_page:1,//当前页码
+                size:15,//每页的数量
+                busName:'searchAlarm',
+                allCounts:0,
+                tableHead:[],
+                tableData:[],
+                formConditionsArr:[
+                    {
+                        label:'名称',
+                        paramName:'alert_name',
+                        model:{
+                            model:''
+                        },
+                        itemType:'',
+                        type:'input'
+                    }
+                ],
+                conditionFrom:{
+                    alert_name:''
+                }
             }
+        },
+        created(){
+            this.tableHead=[
+                {
+                    prop:'alert_name',
+                    label:'名称',
+                    width:''
+                },
+                {
+                    prop:'template_name',
+                    label:'数据源',
+                    width:'',
+                    formatData:(val,obj)=>{
+                        return obj.pre_index_name+obj.suffix_index_name
+                    }
+                },
+                {
+                    prop:'alert_conditions',
+                    label:'告警条件',
+                    width:'',
+                    formatData:(val,obj)=>{
+                        let str = '';
+                        let arr = JSON.parse(val);
+                        for(let i in arr){
+                            if(arr[i].operator !== 'is one of' && arr[i].operator !== 'is not one of'){
+                                str += `${arr[i].field} : ${arr[i].operator}<b> ${arr[i].value}</b>`
+                            }else{
+                                str += `${arr[i].field} : ${arr[i].operator}<b> ${arr[i].values}</b>`
+                            }
+                        }
+                        return str
+                    }
+                },
+                {
+                    prop:'alert_cron',
+                    label:'执行周期',
+                    width:''
+                },
+                {
+                    prop:'alert_time',
+                    label:'时间周期',
+                    width:'',
+                    formatData:(val,obj)=>{
+                        let time = '';
+                        //判断时间类型
+                        if(obj.alert_time_type === 'range'){//时间范围
+                            let arr = val.split(',');
+                            if(arr[0] === ''){
+                                arr[0] = '*'
+                            }
+                            if(arr[1] === ''){
+                                arr[1] = '*'
+                            }
+                            time = arr[0]+'至'+ arr[1]
+                        }else{ //最近....
+                            //time = val
+                            if(val === '1-daying'){
+                                time = '今天'
+                            }else if(val === '1-weeking'){
+                                time = '这周'
+                            }else if(val === '1-monthing'){
+                                time = '本月'
+                            }else{
+                                let arr = val.split('-');
+                                if(arr[1] === 'min'){
+                                    time ='最近'+arr[0]+'分钟'
+                                }else if(arr[1] === 'hour'){
+                                    time ='最近'+arr[0]+'小时'
+                                }else if(arr[1] === 'day'){
+                                    time ='最近'+arr[0]+'天'
+                                }else{
+                                    time = '时间格式未知'
+                                }
+                            }
+                        }
+                        return time
+                    }
+                },
+                {
+                    prop:'alert_state',
+                    label:'状态',
+                    width:'80',
+                    formatData:(val,obj)=>{
+                        let str = ''
+                        if(val){
+                           // str = '<el-switch v-model="aa" active-color="#13ce66" inactive-color="#ff4949"> </el-switch>'
+                            str='<label><input class="mui-switch" type="checkbox" checked></label>'
+                        }else{
+                            str='<label><input class="mui-switch mui-switch-anim" type="checkbox"></label>'
+                        }
+                        return str
+                    },
+                    clickFun:(item,i)=>{
+                        this.setAlarmState(item,i)
+                    }
+                },
+                {
+                    prop:'alert_note',
+                    label:'说明',
+                    width:''
+                },
+                {
+                    prop:'tools',
+                    label:'操作',
+                    width:'',
+                    btns:[
+                        {
+                            icon:'el-icon-edit',
+                            text:'修改',
+                            clickFun:(row,index)=>{
+                                this.goToReviseAlarm(row,index)
+                            }
+                        },
+                        {
+                            icon:'el-icon-close',
+                            text:'删除',
+                            clickFun:(row,index)=>{
+                                this.deleteAlarm(row.alert_id)
+                            }
+                        }
+                    ]
+                }
+            ]
+           /* this.tableData=[
+                {
+                    alert_id:'12',
+                    alert_name:'告警测试',
+                    template_name:'winlogbeat',
+                    pre_index_name:'winlogbeat-',
+                    suffix_index_name:'*',
+                    alert_state:true,
+                    datefield:'',
+                    alert_search_filters:'',
+                    alert_cron:'0 *!/2 * * * ?',
+                    alert_note:'',
+                    alert_search_metric:[],
+                    alert_search_bucket:[],
+                    alert_conditions:"[{\"field\":\"count\",\"fieldType\":\"string\",\"operator\":\">=\",\"value\":\"1000\",\"start\":\"\",\"end\":\"\",\"values\":[]}]",
+                    alert_structure:'',
+                    alert_time_last:'15-min',
+                    alert_time_start_end:''
+                },{
+                    alert_id:'34',
+                    alert_name:'告警测试2',
+                    template_name:'winlogbeat',
+                    pre_index_name:'winlogbeat-',
+                    suffix_index_name:'*',
+                    alert_state:false,
+                    datefield:'',
+                    alert_search_filters:'',
+                    alert_cron:'0 *!/2 * * * ?',
+                    alert_note:'',
+                    alert_search_metric:[],
+                    alert_search_bucket:[],
+                    alert_conditions:"[{\"field\":\"count\",\"fieldType\":\"string\",\"operator\":\"is one of\",\"value\":\"\",\"start\":\"\",\"end\":\"\",\"values\":[100,200,300]}]",
+                    alert_structure:'',
+                    alert_time_last:'',
+                    alert_time_start_end:'2020-10-01 00:00:00,2020-11-01 00:00:00'
+                }
+            ]*/
+            this.getAlarmList(this.conditionFrom,1)
+            bus.$on(this.busName,(params)=>{
+                this.conditionFrom = params;
+                this.getAlarmList(this.conditionFrom,1)
+                this.c_page = 1;
+            })
+        },
+        beforeDestroy(){
+            bus.$off(this,busName)
         },
         methods:{
             /*添加告警*/
@@ -24,12 +228,180 @@
             },
             /*刷新*/
             refresh(){
+                this.c_page = 1;
+                this.getAlarmList(this.conditionFrom,1);
+            },
+            /*获得告警列表*/
+            getAlarmList(searchObj,page){
+                this.loading = true;
+                let obj = searchObj;
+                obj.pageIndex = page;//当前页
+                obj.pageSize = this.size;//页的条数
+                this.$nextTick(()=>{
+                    this.loading = true;
+                    this.$axios.post(this.$baseUrl+'/alert/getAlertInfoByCondition.do',this.$qs.stringify(searchObj))
+                        .then(res=>{
+                            this.loading = false;
+                            let obj = res.data;
+                            if(obj.success === 'true'){
+                                this.allCounts = Number(obj.data.count);
+                                this.tableData = obj.data.list;
+                            }else{
+                                layer.msg(obj.message,{icon:5})
+                            }
+                        })
+                        .catch(err=>{
+                             this.loading = false;
+                        })
+                })
+            },
+            /*修改告警*/
+            goToReviseAlarm(item){
+                jumpHtml('editAlarm'+item.alert_id,'alarmManage/editAlarm.vue',{ name:item.alert_name,id:item.alert_id },'修改')
+            },
+            /*删除告警*/
+            deleteAlarm(id){
+                //询问框
+                layer.confirm('您确定删除么？', {
+                    btn: ['确定','取消'] //按钮
+                }, (index)=>{
+                    this.loading = true
+                    this.$nextTick(()=>{
+                        this.$axios.post(this.$baseUrl+'/',this.$qs.stringify({alert_id:id}))
+                            .then((res)=>{
+                                this.loading = false
+                                if(res.data.success == "true"){
+                                    layer.msg("删除成功",{icon:1});
+                                    this.c_page = 1;
+                                    this.getAlarmList(this.conditionFrom,1)
+                                }else{
+                                    layer.msg(res.data.message,{icon:5});
+                                }
 
+                            })
+                            .catch((err)=>{
+                                this.loading = false;
+                                layer.msg("删除失败",{icon:5});
+                            })
+                    })
+                }, function(){
+                    layer.close();
+                })
+            },
+            /*分页页码改变*/
+            handleCurrentChange(page){
+               //获取数据
+                this.getAlarmList(this.conditionFrom,page);
+                //改变标识
+                //this.firstGetData = false;
+            },
+            /*设置告警状态*/
+            setAlarmState(item,i){
+                this.$nextTick(()=>{
+                    this.loading = true;
+                    this.$axios.post(this.$baseUrl+'/',this.$qs.stringify({alert_id:item.alert_id}))
+                        .then(res=>{
+                            this.loading = false;
+                            let obj = res.data;
+                            if(obj.success === 'true'){
+                                layer.msg(obj.message,{icon:5})
+                                this.tableData[i].alert_state = !item.alert_state
+                            }else{
+                                layer.msg(obj.message,{icon:5})
+                            }
+                        })
+                        .catch(err=>{
+                             this.loading = false;
+                        })
+                })
             }
+        },
+        components:{
+            vBasetable,
+            vSearchForm
         }
     }
 </script>
 
 <style scoped>
+    .btn-wapper{
+        float: right;
+        margin-right: 10px;
+    }
+    .alarm-list-wapper{
+        padding: 10px;
+    }
+    .alarm-table-page{
+        border-top: 1px solid #303e4e;
+        height: 40px;
+        display: flex;
+        justify-content: flex-end;
+        align-items:center;
+    }
+    .alarm-table-page b{
+        color: #e4956d;
+    }
+    /deep/ .mui-switch {
+        width: 42px;
+        height: 21px;
+        position: relative;
+        border: 1px solid #ff4949;
+        background-color: #ff4949;
+        box-shadow: #dfdfdf 0 0 0 0 inset;
+        border-radius: 20px;
+        border-top-left-radius: 20px;
+        border-top-right-radius: 20px;
+        border-bottom-left-radius: 20px;
+        border-bottom-right-radius: 20px;
+        background-clip: content-box;
+        display: inline-block;
+        -webkit-appearance: none;
+        user-select: none;
+        outline: none; }
+    /deep/ .mui-switch:before {
+        content: '';
+        width: 20px;
+        height: 20px;
+        position: absolute;
+        top: 0px;
+        left: 0;
+        border-radius: 20px;
+        border-top-left-radius: 20px;
+        border-top-right-radius: 20px;
+        border-bottom-left-radius: 20px;
+        border-bottom-right-radius: 20px;
+        background-color: #fff;
+        box-shadow: 0 1px 3px rgba(0, 0, 0, 0.4); }
+    /deep/ .mui-switch:checked {
+        border-color: #64bd63;
+        box-shadow: #64bd63 0 0 0 16px inset;
+        background-color: #64bd63; }
+    /deep/ .mui-switch:checked:before {
+        left: 21px; }
+    /deep/ .mui-switch.mui-switch-animbg {
+        transition: background-color ease 0.4s; }
+    /deep/ .mui-switch.mui-switch-animbg:before {
+        transition: left 0.3s; }
+    /deep/ .mui-switch.mui-switch-animbg:checked {
+        box-shadow: #dfdfdf 0 0 0 0 inset;
+        background-color: #64bd63;
+        transition: border-color 0.4s, background-color ease 0.4s; }
+    /deep/ .mui-switch.mui-switch-animbg:checked:before {
+        transition: left 0.3s;
+    }
+    /deep/ .mui-switch.mui-switch-anim {
+        transition: border cubic-bezier(0, 0, 0, 1) 0.4s, box-shadow cubic-bezier(0, 0, 0, 1) 0.4s;
+    }
+    /deep/ .mui-switch.mui-switch-anim:before {
+        transition: left 0.3s;
+    }
+    /deep/ .mui-switch.mui-switch-anim:checked {
+        box-shadow: #64bd63 0 0 0 16px inset;
+        background-color: #64bd63;
+        transition: border ease 0.4s, box-shadow ease 0.4s, background-color ease 1.2s;
+    }
+    /deep/ .mui-switch.mui-switch-anim:checked:before {
+        transition: left 0.3s;
+    }
 
 </style>

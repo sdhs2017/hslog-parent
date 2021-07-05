46d86e75e03121abb7a71e31f1c128b56bca1a8f
15069109870 <553047805@qq.com>
2021-07-05 16:55:06 +0800

    验证icense 修改，添加一个获取license信息的方法

--- hslog/src/main/java/com/jz/bigdata/common/license/VerifyLicense.java
+++ hslog/src/main/java/com/jz/bigdata/common/license/VerifyLicense.java
@@ -24,79 +24,117 @@ import de.schlichtherle.license.LicenseManager;
 public class VerifyLicense {
 
 	//common param
-		private static String PUBLICALIAS = "";
-		private static String STOREPWD = "";
-		private static String SUBJECT = "";
-		private static String licPath = "";
-		private static String pubPath = "";
-		
-		
-		public void setParam(String propertiesPath) {
-			// 获取参数
-			Properties prop = new Properties();
-			InputStream in = getClass().getResourceAsStream(propertiesPath);
-			try {
-				prop.load(in);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			PUBLICALIAS = prop.getProperty("PUBLICALIAS");
-			STOREPWD = prop.getProperty("STOREPWD");
-			SUBJECT = prop.getProperty("SUBJECT");
-			licPath = prop.getProperty("licPath");
-			pubPath = prop.getProperty("pubPath");
-		}
+    private static String PUBLICALIAS = "";
+    private static String STOREPWD = "";
+    private static String SUBJECT = "";
+    private static String licPath = "";
+    private static String pubPath = "";
 
-		public boolean verify() {		
-			/************** 证书使用者端执行 ******************/
 
-			LicenseManager licenseManager = LicenseManagerHolder
-					.getLicenseManager(initLicenseParams());
-			// 安装证书
-			try {
-				//licenseManager.install(new File(licPath));
-				File file = new File(getClass().getClassLoader().getResource(licPath).getFile());
-				System.out.println(file.getAbsolutePath());
-				licenseManager.install(file);
-				System.out.println("客户端安装证书成功!");
-			} catch (Exception e) {
-				e.printStackTrace();
-				System.out.println("客户端证书安装失败!");
-				return false;
-			}
-			// 验证证书
-			try {
-				LicenseContent ss = licenseManager.verify();
-				DateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-				Date date = ss.getIssued();
-				ss.getNotAfter();
-				System.out.println(format.format(date));
-				System.out.println("有效期："+format.format(ss.getNotBefore())+"-"+format.format(ss.getNotAfter()));
-				System.out.println("客户端验证证书成功!");
-			} catch (Exception e) {
-				e.printStackTrace();
-				System.out.println("客户端证书验证失效!");
-				return false;
-			}
-			return true;
-		}
+    public void setParam(String propertiesPath) {
+        // 获取参数
+        Properties prop = new Properties();
+        InputStream in = getClass().getResourceAsStream(propertiesPath);
+        try {
+            prop.load(in);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        PUBLICALIAS = prop.getProperty("PUBLICALIAS");
+        STOREPWD = prop.getProperty("STOREPWD");
+        SUBJECT = prop.getProperty("SUBJECT");
+        licPath = prop.getProperty("licPath");
+        pubPath = prop.getProperty("pubPath");
+    }
 
-		// 返回验证证书需要的参数
-		private static LicenseParam initLicenseParams() {
-			Preferences preference = Preferences
-					.userNodeForPackage(VerifyLicense.class);
-			CipherParam cipherParam = new DefaultCipherParam(STOREPWD);
+    public boolean verify() {
+        /************** 证书使用者端执行 ******************/
 
-			KeyStoreParam privateStoreParam = new DefaultKeyStoreParam(
-					VerifyLicense.class, pubPath, PUBLICALIAS, STOREPWD, null);
-			LicenseParam licenseParams = new DefaultLicenseParam(SUBJECT,
-					preference, privateStoreParam, cipherParam);
-			return licenseParams;
-		}
-		
-		public static void main(String [] args) {
-			File file = new File(new VerifyLicense().getClass().getClassLoader().getResource("").getFile());
-			System.out.println(file.getAbsolutePath());
-		}
+        LicenseManager licenseManager = LicenseManagerHolder
+                .getLicenseManager(initLicenseParams());
+        // 安装证书
+        try {
+            //licenseManager.install(new File(licPath));
+            File file = new File(getClass().getClassLoader().getResource(licPath).getFile());
+            System.out.println(file.getAbsolutePath());
+            licenseManager.install(file);
+            System.out.println("客户端安装证书成功!");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("客户端证书安装失败!");
+            return false;
+        }
+        // 验证证书
+        try {
+            LicenseContent ss = licenseManager.verify();
+            DateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            Date date = ss.getIssued();
+            ss.getNotAfter();
+            System.out.println(format.format(date));
+            System.out.println("有效期："+format.format(ss.getNotBefore())+"-"+format.format(ss.getNotAfter()));
+            System.out.println("客户端验证证书成功!");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("客户端证书验证失效!");
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * 获取license 内容信息
+     * @return
+     */
+    public LicenseContent getLicenseContent() {
+        /************** 证书使用者端执行 ******************/
+
+        LicenseManager licenseManager = LicenseManagerHolder
+                .getLicenseManager(initLicenseParams());
+        // 安装证书
+        try {
+            //licenseManager.install(new File(licPath));
+            File file = new File(getClass().getClassLoader().getResource(licPath).getFile());
+            System.out.println(file.getAbsolutePath());
+            licenseManager.install(file);
+            System.out.println("客户端安装证书成功!");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("客户端证书安装失败!");
+
+        }
+        // 验证证书
+        try {
+            LicenseContent licenseContent = licenseManager.verify();
+            DateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            Date date = licenseContent.getIssued();
+            licenseContent.getNotAfter();
+
+            System.out.println(format.format(date));
+            System.out.println("有效期："+format.format(licenseContent.getNotBefore())+"-"+format.format(licenseContent.getNotAfter()));
+            System.out.println("客户端验证证书成功!");
+            return licenseContent;
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("客户端证书验证失效!");
+        }
+        return null;
+    }
+    // 返回验证证书需要的参数
+    private static LicenseParam initLicenseParams() {
+        Preferences preference = Preferences
+                .userNodeForPackage(VerifyLicense.class);
+        CipherParam cipherParam = new DefaultCipherParam(STOREPWD);
+
+        KeyStoreParam privateStoreParam = new DefaultKeyStoreParam(
+                VerifyLicense.class, pubPath, PUBLICALIAS, STOREPWD, null);
+        LicenseParam licenseParams = new DefaultLicenseParam(SUBJECT,
+                preference, privateStoreParam, cipherParam);
+        return licenseParams;
+    }
+
+    public static void main(String [] args) {
+        File file = new File(new VerifyLicense().getClass().getClassLoader().getResource("").getFile());
+        System.out.println(file.getAbsolutePath());
+    }
 }
